bc6b0dd239e915e38afe299b795731a6b02f17c7
Edson Mendes <124837899+EdsonKL@users.noreply.github.com>
2024-05-22 15:28:56 -0300

    finalização carrinho de compra

--- src/app/components/Cart/index.tsx
+++ src/app/components/Cart/index.tsx
@@ -6,14 +6,17 @@ import { appContext, AppContextType } from "@/app/contexts/context";
 
 function Cart() {
   // Context para receber dados do carrinho e alterar status de exibição do carrinho
-  const { cartItems, cartIsVisible, setCartIsVisible } =
-    useContext<AppContextType>(appContext);
+  const { setCartItems, cartItems, cartIsVisible, setCartIsVisible } = useContext<AppContextType>(appContext);
 
   // Função para fechar o carrinho
   const closeCart = (): void => {
     setCartIsVisible(false);
     console.log(cartItems);
   };
+
+  const totalPrice = cartItems.reduce((acc, price) => {
+    return parseInt(price.price) * price.quantity + acc;
+  }, 0);
 
   return (
     <section
@@ -25,23 +28,33 @@ function Cart() {
         <h1>Carrinho de compras</h1>
         <button onClick={() => closeCart()}>X</button>
       </div>
-      {/* Renderização dos items do carrinho */}
-      {cartItems ? 
-        cartItems.map((cartItem: any) => (
-          <CartItem
-            key={cartItem.id}
-            id={cartItem.id}
-            photo={cartItem.photo}
-            title={cartItem.name}
-            price={cartItem.price * cartItem.quantity}
-            quantity={cartItem.quantity}
-          />
-        ))
-        : (
-        <h2>não há itens...</h2>
-      )}
+      <div className={styles.cartItems}>
+        {/* Renderização dos items do carrinho */}
+        {cartItems.length > 0 ? (
+          cartItems.map((cartItem: any) => (
+            <CartItem
+              key={cartItem.id}
+              id={cartItem.id}
+              photo={cartItem.photo}
+              title={cartItem.name}
+              price={cartItem.price * cartItem.quantity}
+              quantity={cartItem.quantity}
+            />
+          ))
+        ) : (
+          <h2 className={styles.withoutItems}>Não há itens no carrinho...</h2>
+        )}
+      </div>
       {/* Resumo da compra */}
-      <div>.</div>
+      <div className={styles.CartFooter}>
+        <div className={styles.totalPrice}>
+          <p>Total:</p>
+          <span>R$ {totalPrice}</span>
+        </div>
+        <div className={styles.finishShop}>
+          <button onClick={() => setCartItems([])}>Finalizar Compra</button>
+        </div>
+      </div>
     </section>
   );
 }
